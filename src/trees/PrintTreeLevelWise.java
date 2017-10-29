d572b41095a6b6c3faf1bed8c6f7667a75b34acd
Arvind Sharma <avin001@gmail.com>
2017-10-29 14:59:08 +0530

    Added new method numNodes in the file PrintTreeLevelWise.java
    Added new method numNodes in the file PrintTreeLevelWise.java

--- src/trees/PrintTreeLevelWise.java
+++ src/trees/PrintTreeLevelWise.java
@@ -55,6 +55,17 @@ public class PrintTreeLevelWise {
 		}
 	}
 	
+	public static int numNodes(TreeNode<Integer> root) {
+		if (root == null) {
+			return 0;
+		}
+		int count = 1;
+		for (int i = 0; i < root.children.size(); i++) {
+			count += numNodes(root.children.get(i));
+		}
+		return count;
+	}
+	
 	public static void main(String[] args) {
 		/*
 		 Given a generic tree, print the input tree in level wise order. That is, 
@@ -76,6 +87,7 @@ public class PrintTreeLevelWise {
 		 */
 		TreeNode<Integer> root = takeInputLevelWise();
 		printLevelWise(root);
+		System.out.printf("There are %d nodes in the tree.%n", numNodes(root));
 	}
 
 }
