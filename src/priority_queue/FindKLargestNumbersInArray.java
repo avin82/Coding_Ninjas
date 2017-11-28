1d9b29b0b849f9f10553fca19f50362f6ce76820
Arvind Sharma <avin001@gmail.com>
2017-11-29 03:38:59 +0530

    Added new file FindKLargestNumbersInArray.java
    Added new file FindKLargestNumbersInArray.java

---
+++ src/priority_queue/FindKLargestNumbersInArray.java
@@ -0,0 +1,91 @@
+package priority_queue;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class MaxPQComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		if (o1 < o2) {
+			return -1;
+		} else if (o1 > o2) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
+
+public class FindKLargestNumbersInArray {
+	
+	public static int[] takeInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter the size of the array: ");
+		int size = scanner.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Please enter the elements of the array separated by space or return: ");
+		for (int i = 0; i < size; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		return arr;
+	}
+	
+	public static ArrayList<Integer> findkLargestInArray(int input[], int k) {
+		MaxPQComparator maxPQComparator = new MaxPQComparator();
+		PriorityQueue<Integer> pQueue = new PriorityQueue<>(maxPQComparator);
+		ArrayList<Integer> output = new ArrayList<>();
+		for (int i = 0; i < input.length; i++) {
+			pQueue.add(input[i]);
+		}
+		while (pQueue.size() != k) {
+			pQueue.remove();
+		}
+		while (!pQueue.isEmpty()) {
+			output.add(pQueue.remove());
+		}
+		return output;
+	}
+	
+	public static void main(String[] args) {
+		/*
+		 You are given with an integer k and an array of integers that contain numbers in random order. 
+		 Write a program to find k largest numbers from given array. You need to save them in an array 
+		 and return it. 
+		 
+		 Time complexity should be O(nlogk) and space complexity should be not more than O(k). 
+		 
+		 Order of elements in the output is not important. 
+		 
+		 Input Format : 
+		 Line 1 : Size of array (n) 
+		 Line 2 : Array elements (separated by space) 
+		 Line 3 : Integer k 
+		 
+		 Output Format : 
+		 k largest elements 
+		 
+		 Sample Input : 
+		 13 
+		 2 12 9 16 10 5 3 20 25 11 1 8 6 
+		 4 
+		 
+		 Sample Output : 
+		 12 
+		 16 
+		 20 
+		 25	
+		 */
+		int[] arr = takeInput();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter an integer k to find k largest elements from the given array: ");
+		int k = scanner.nextInt();
+		ArrayList<Integer> list = findkLargestInArray(arr, k);
+		for (Integer element : list) {
+			System.out.println(element);
+		}
+	}
+
+}
